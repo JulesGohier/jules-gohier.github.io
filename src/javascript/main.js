45cea8096a0baa6320f0325134bf170c4258f441
JulesGohier <jules.reihog5@gmail.com>
2024-03-06 21:41:30 +0100

    jfdfs

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -22,3 +22,53 @@ function copy(){
 function download(){
     browser.downloads("../assets/icons/Icon-Start.png")
 }
+
+
+const state = {};
+const carouselList = document.querySelector(".projects-carousel-list");
+const carouselItems = document.querySelectorAll(".projects-carousel-list-item");
+const elems = Array.from(carouselItems);
+
+
+carouselList.addEventListener("click", function (event) {
+  var newActive = event.target.closest(".projects-carousel-list-item");
+
+  var isItem = newActive.closest(".projects-carousel-list-item");
+
+  if (!isItem) {
+    return;
+  }
+  
+  update(newActive);
+});
+
+const update = function (newActive) {
+  const newActivePos = newActive.dataset.position;
+
+  const current = elems.find((elem) => elem.dataset.position == 0);
+  const prev = elems.find((elem) => elem.dataset.position == -1);
+  const next = elems.find((elem) => elem.dataset.position == 1);
+  const first = elems.find((elem) => elem.dataset.position == -2);
+  const last = elems.find((elem) => elem.dataset.position == 2);
+
+    console.log([current, prev, next, first, last]);
+
+  [current, prev, next, first, last].forEach((item) => {
+    var itemPos = item.dataset.position;
+
+    item.dataset.position = getPos(itemPos, newActivePos);
+    console.log(itemPos, item.dataset.position);
+  });
+};
+
+const getPos = function (current, active) {
+  const diff = current - active;
+
+  
+
+  if (Math.abs(current - active) > 2) {
+    return -current;
+  }
+
+  return diff;
+};
